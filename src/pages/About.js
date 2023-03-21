daedcf1f08ea12fb36ea989aaca47f5114d743d0
Ivan Letov <letov88@gmail.com>
2023-03-21 12:32:52 +0200

    About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import '../styles/Pages.css'
 import '../styles/About.css'
+import { Container, Row, Col } from 'react-bootstrap'
 import mission from '../images/services-5.png.webp'
 import vision from '../images/services-6.png.webp'
+import profilePic from '../images/profile-pic.jpg'
 
 function About() {
 	return (
 		<>
 			<div className='multyPageWrap'>
-
+				<h1>About Me</h1>
 			</div>
 
-			<div className='container'>
-			{/*---main row---*/}
-				<div className='row'>
-				{/*------------------------------------------*/}
-					<div className='col-lg-4 col-md-4 about-col-left'>
-
+			<Container>
+			{/*----------section one---------*/}
+				<Row className='first-about-row'>
+				
+					<Col lg={4} md={4} className='about-col-left'>
 						<div className='section-title'>
 							<p>who we are</p>
 							<h1 >Lorem ipsum dolor sit amet</h1>
@@ -26,31 +27,52 @@ function About() {
 							<p className='fw-light'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
 							</p>
 						</div>
-					</div>
-					{/*---------------------------------------*/}
-					<div className='col-lg-8 col-md-8 about-col-right'>
+					</Col>
+				
+					<Col lg={8} md={8} className='about-col'>
 						<div className='about-page-services'>
 							<p className='fw-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 							</p>
 						</div>
-						{/*------inner row------*/}
-						<div className='row inner-row'>
-							<div className='col-lg-6 col-md-6'>
+				
+						<Row className='inner-row'>
+							<Col lg={6} md={6}>
 								<img src={mission} alt='...' />
 								<h4>Mision</h4>
 								<p className='fw-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
 								</p>
-							</div>
-							<div className='col-lg-6 col-md-6'>
+							</Col>
+							<Col lg={6} md={6}>
 								<img src={vision} alt='...' />
 								<h4>Vision</h4>
 								<p className='fw-light'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium
 								</p>
-							</div>
-						</div>
-					</div>
+							</Col>
+						</Row>
+					</Col>
+				</Row>
+				</Container>
+
+				<div className='multyPageWrap second-about'>
+					<p>why choose me</p>
+					<h1>My ability to bring outstanding results</h1>
 				</div>
-			</div>	
+
+				{/*------------section two-------------*/}
+				<Container>
+				<Row className='about-second-row'>
+					<Col lg={8} md={8} className='about-col'>
+						<h2>Something about me</h2>
+						<p className='fw-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium
+						</p>
+					</Col>
+					<Col lg={4} md={4}  className='about-two-right'>
+						<img src={profilePic} alt='...' />
+					</Col>
+				</Row>
+				</Container>
+
+
 		</>
 	)
 }
